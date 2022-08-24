f872e1811a0ba68c87b55ef8b0b5d0d9a503875e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-08-23 19:43:32 -0700

    Fix 1 for task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(i + 1, 10):
         num = (i * 10) + j
         if num != 89:
-            print("{0:02d}".format(num), end =", ")
+            print("{0:02d}".format(num), end=", ")
         else:
             print("{0:02d}".format(num))
