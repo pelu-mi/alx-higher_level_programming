78ff6b45c4bf39009bd74632ea8c6f50dbfb0137
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-08-23 21:40:01 -0700

    Fix 1 for task 11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b = 0:
+        return 1
+    if b > 0:
+        i = 0
+        prod = 1
+        while i < b:
+            prod = prod * a
+            i += 1
+        return prod
+    if b < 0:
+        return abs(a ** b)
