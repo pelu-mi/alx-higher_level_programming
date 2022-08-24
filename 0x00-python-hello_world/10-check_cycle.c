da92d2e11db1b3fd124cd0258cb6905bcbde462b
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-08-24 08:35:18 -0700

    Extra touches

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * check_cycle - function to check for cycles in a linked list
+ * @list: list to check for cycle
+ * Return: 1 if cycle, 0 if not
+ */
+
 int check_cycle(listint_t *list)
 {
 	while (list)
