0b703053c9421c07006d06a41caef10b118a973e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-08-25 21:02:07 -0700

    Added file for task 5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    # stuff only to run when not called via 'import' here
+    from variable_load_5 import a
+
+    print(a)
+    main()
