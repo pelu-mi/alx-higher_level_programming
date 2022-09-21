a889e8d64cb44a7a5ad894772af65eb5a2dd1c4c
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-21 16:32:53 +0100

    Fix 1 for task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    product=[[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
+    product = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             product[i][j] = matrix[i][j] ** 2
