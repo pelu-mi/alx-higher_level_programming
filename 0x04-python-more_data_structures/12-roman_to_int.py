c256f054527625ef4fbbc0294b2d48a527e29691
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-21 19:29:14 +0100

    Added file for task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or roman_string is str:
+        return 0
+    num = 0
+    prev = ''
+    for i in roman_string:
+        if i == 'M':
+            num += 1000
+        if i == 'D':
+            if prev == 'C':
+                num += 300
+            else:
+                num += 500
+        if i == 'C':
+            if prev == 'X':
+                num += 80
+            else:
+                num += 100
+        if i == 'L':
+            if prev == 'X':
+                num += 30
+            else:
+                num += 50
+        if i == 'X':
+            if prev == 'I':
+                num += 8
+            else:
+                num += 10
+        if i == 'V':
+            if prev == 'I':
+                num += 3
+            else:
+                num += 5
+        if i == 'I':
+            num += 1
+
+        prev = i
+    return num
