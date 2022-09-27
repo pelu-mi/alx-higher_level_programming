b6e7ae252b8af45fe5c88df2416cea6e08bde8fd
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-27 23:27:26 +0100

    Fix 1 for task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,13 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    try:
-        result = 0
-        for i in range(1, 3):
-    except:
-        if i > a:
-            raise Exception('Too Far')
-
-        result = result + ((a ** b) / i)
-    finally:
-        result = b + a
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too Far')
+            else:
+                result = result + ((a ** b) / i)
+        except Exception:
+            result = b + a
+            break
     return result
