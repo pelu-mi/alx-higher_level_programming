aa535e361ead781b1806a916db77383336a1036e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-30 14:53:33 +0100

    Fix 1 for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,7 +6,7 @@
 class Square:
     """This is a class that defines Square with a size
     """
-    
+
     def __init__(self, size):
         """To initialize the class, you need one variable
         Args:
