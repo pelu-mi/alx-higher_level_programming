eb654802dd6bdef850a8f85a35e44090734ad4d8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-30 14:51:37 +0100

    Added file for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This Module is for a class Square
+"""
+
+
+class Square:
+    """This is a class that defines Square with a size
+    """
+    
+    def __init__(self, size):
+        """To initialize the class, you need one variable
+        Args:
+            size (int): size of the square.
+        """
+        self.__size = size
