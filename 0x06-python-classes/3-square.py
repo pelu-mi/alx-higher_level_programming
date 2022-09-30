795afbd9f49a65f9934b5058f529de4a4503c2b0
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-09-30 15:05:41 +0100

    Added file for task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This Module is for a class Square"""
+
+
+class Square:
+    """This is a class that defines Square with a size"""
+
+    def __init__(self, size=0):
+        """To initialize the class, you need one variable
+        Args:
+            size (int): size of the square.
+        """
+        if (not (isinstance(size, int))):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """This function returns the area of the square"""
+        return self.__size ** 2
