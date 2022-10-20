54091b8c327bf4a76ac04a085edf7ae881c67556
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-10-20 08:04:41 +0100

    Fix 1 for task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,18 +13,17 @@ def matrix_divided(matrix, div):
     Returns:
         List: Matrix with each element divided by divisor
     """
-    if not isinstance(matrix, list):# check if matrix is a list
-        raise TypeError("matrix must be a matrix (list of lists) of
-                integers/floats")
+    message1 = "matrix must be a matrix (list of lists) of integers/floats"
+    row_matrix = len(matrix[0])
+    if not isinstance(matrix, list):  # check if matrix is a list
+        raise TypeError(message1)
     for row in range(0, len(matrix)):
-        if not isinstance(matrix[row], list):# check if each element is a list
-            raise TypeError("matrix must be a matrix (list of lists) of
-                    integers/floats")
+        if not isinstance(matrix[row], list):  # check each element is a list
+            raise TypeError(message1)
         r = 0
         for col in matrix[row]:
             if not isinstance(col, int) and not isinstance(col, float):
-                raise TypeError("matrix must be a matrix (list of lists) of
-                        integers/floats")# check if items is int or float
+                raise TypeError(message1)  # check if items is int or float
             r = r + 1
         if r != row_matrix:
             raise TypeError("Each row of the matrix must have the same size")
@@ -32,11 +31,10 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    row_matrix = len(matrix[0])# length of the first row in the matrix
     result = []
     for i in range(0, len(matrix)):
         new_row = []
         for col in matrix[i]:
             new_row.append(round(col/div, 2))
-        result.append(new_row)# Find a simpler way to do this
+        result.append(new_row)  # Find a simpler way to do this
     return result
