bda836a9d187bb7e923fe69b91eb48b40446bc23
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-10-26 11:46:27 +0100

    Added files for task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module to define class Square inheriting from class Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square inheriting from class Rectangle
+    """
+
+    def __init__(self, size):
+        """ Initialize Square as Rectangle with same size
+        """
+        super().__init__(size, size)
+        self.__size = size
