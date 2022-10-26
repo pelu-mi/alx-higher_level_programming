2558b602908cee4b0fdd5baade75255f683b19f3
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-10-26 11:51:29 +0100

    Fix 1 for task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -13,5 +13,6 @@ class Square(Rectangle):
     def __init__(self, size):
         """ Initialize Square as Rectangle with same size
         """
+        self.integer_validator(size)
         super().__init__(size, size)
         self.__size = size
