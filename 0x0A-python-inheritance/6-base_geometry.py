35390f54f76397517065758d28155c6b4f502351
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-10-26 11:02:27 +0100

    Added file for task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module to define class BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """ Class BaseGeometry with an area function
+    """
+    
+    def area(self):
+        """ Function to raise an exception
+        """
+        raise Exception("area() is not implemented")
