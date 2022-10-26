5504bcf6824fca4a4a3cd381cd0195f667db3f79
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-10-26 10:08:27 +0100

    Added file for task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module to define lookup function
+"""
+
+
+def lookup(obj):
+    """ Function to return list of members of an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+This Readme is for python inheritance
