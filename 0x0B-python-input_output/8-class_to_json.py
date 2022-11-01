85e7831cea8e528a65e9e28b0e5ebded2a122ad2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-01 20:34:17 +0100

    Added file for task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module Documentation
+"""
+
+
+def class_to_json(obj):
+    """ Function to return dictionary representation of an object instance
+    """
+    return obj.__dict__
