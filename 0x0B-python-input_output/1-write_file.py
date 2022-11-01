3af152551b854b534887fefa0c2f385dc73ab808
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-01 18:44:22 +0100

    Added file for task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Module Documentation
+"""
+
+
+def write_file(filename="", text=""):
+    """ Function to write text to a file with UTF8 encoding and
+    return the number of chars written
+
+    Args:
+        filename (str): Name of file to write to
+        text (str): String to write into file
+
+    Returns:
+        Total number of characters written
+    """
+    chars = 0
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
