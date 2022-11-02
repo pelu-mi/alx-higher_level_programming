3743adbcf07c4cd34c5e9c7ef5491f6af3e05ffc
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-02 21:56:55 +0100

    Added file for task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,20 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """ Update instance variables depending on input values
+        """
+        if len(args) != 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            for key in sorted(kwargs):
+                setattr(self, key, kwargs[key])
