d9b16a73a5de22c4bf00f0dc31748ebd44ad1d53
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-02 21:01:32 +0100

    Betty fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -94,4 +94,5 @@ class Rectangle(Base):
     def __str__(self):
         """ Format output for Rectangle
         """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.x, self.y, self.width, self.height)
