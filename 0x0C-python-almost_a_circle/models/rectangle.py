6f1c962b408d1bda2b25847fb57047aaff14f9ba
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-02 21:00:21 +0100

    Added file for task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,3 +90,8 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        """ Format output for Rectangle
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
