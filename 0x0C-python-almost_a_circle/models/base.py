cd24e2b38fe5ca9b945b0e0ca8475956985f545d
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-03 15:33:44 +0100

    Added update for task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -52,3 +52,17 @@ class Base:
             else:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Static method to convert json string to list of objects
+
+        Args:
+            json_string (str): Json representation of list of objects
+
+        Returns:
+            list: Empty list or list of dictionaries of instances of objects
+        """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
