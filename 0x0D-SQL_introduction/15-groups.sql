8dfb0aea16129eac2f4c41aa6a26b697d659762f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-29 09:02:13 -0800

    Fix 1 for task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Query to list the number of records with the same score in the table
-SELECT score, COUNT(*) AS 'number' FROM second_table
+SELECT score, COUNT(*) AS `number` FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY `number` DESC;
