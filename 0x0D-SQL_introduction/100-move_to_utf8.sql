eac2e1a3216a77d76e18268787981c41ffc37591
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-29 10:11:58 -0800

    Fix 2 for task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,5 @@
 -- Change Encoding of database, table and field to UTF8
 USE `hbtn_0c_0`
--- Change encoding of table as it is the only content of the db
-ALTER TABLE first_table
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_general_ci;
