566149634713390a764af5c56b79e23c65f672da
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-29 10:37:09 -0800

    Added file for task 19

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- SQL script that displays the average temperature (Fahrenheit)
+-- by city ordered by temperature (descending)
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
