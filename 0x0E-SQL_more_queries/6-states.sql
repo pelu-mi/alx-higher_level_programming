f8c78babfb1963dfdad1a9e9337dc382388e4962
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-30 11:38:31 -0800

    Hot fix for task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script to create database and table with constraint
-CREATE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 id INT NOT NULL AUTO_INCREMENT,
