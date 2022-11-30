26774f1aa8b14de5e0d86eec0ddef385a1e4086f
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-30 14:34:00 -0800

    Fix 1 for task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,10 @@
 -- Query using JOIN statement
-SELECT tv_genres.name AS genre
+SELECT tv_genres.name AS name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'
-GROUP BY genre
-ORDER BY genre ASC;
+GROUP BY name
+ORDER BY name ASC;
