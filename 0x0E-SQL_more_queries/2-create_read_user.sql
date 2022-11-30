2dd086d73494ef6879ad98ed8352d89fde0849d6
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-11-30 11:22:36 -0800

    minor fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Script to Create database and User
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
---CREATE USER
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
