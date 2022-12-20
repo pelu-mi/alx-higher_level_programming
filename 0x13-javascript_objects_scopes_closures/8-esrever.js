0f6e47af087579b553591b98d5f7832d6fd41da7
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-20 17:46:56 +0100

    Added files for task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let rev = [];
+  let len = list.length - 1;
+  let i = 0;
+  while (len >= 0) {
+    rev[i] = list[len];
+    i++;
+    len--;
+  }
+  return rev;
+};
