4d8edf380c21f607907b3aa60de1ac237ad07091
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-20 21:22:48 +0100

    Fix 1 for task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,34 +5,26 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
 
-const data_A;
-const data_B;
-
 fs.readFile(fileA, 'utf8', (err, dataA) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(dataA);
-  data_A = dataA;
+  fs.writeFile(fileC, dataA, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
+
 fs.readFile(fileB, 'utf8', (err, dataB) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(dataB);
-  data_B = dataB;
-});
-
-fs.writeFile(fileC, data_A, err => {
-  if (err) {
-    console.error(err);
-  }
-});
-
-fs.appendFile(fileC, data_B, err => {
-  if (err) {
-    console.error(err);
-  }
+  fs.appendFile(fileC, dataB, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
