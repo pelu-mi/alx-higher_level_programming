c7fbe067c2f75cbe75e57d11d5cd8f020cdd66e2
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-20 03:51:45 +0100

    Fix 3 for task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
 const vars = process.argv.slice(2).map(Number).sort((a, b) => a - b);
-vars.length > 1 ? console.log(Math.min(...vars)) : console.log(0);
+vars.length > 1 ? console.log(vars[vars.length - 2]) : console.log(0);
