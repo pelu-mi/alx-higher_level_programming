3e355b591baf50a4279783e50af101224b089251
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-21 16:36:13 +0100

    pycodestyle fix for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,17 +2,16 @@
 """ Module for script that lists all states from the db hbtn_0e_0_usa
 """
 
-
 if __name__ == "__main__":
     import MySQLdb
     import sys
-
 
     u_name = sys.argv[1]
     pwd = sys.argv[2]
     db = sys.argv[3]
 
-    db = MySQLdb.connect(user=u_name, passwd=pwd, database=db, host='localhost', port=3306)
+    db = MySQLdb.connect(
+            user=u_name, passwd=pwd, database=db, host='localhost', port=3306)
     c = db.cursor()
 
     c.execute("""SELECT * FROM states
