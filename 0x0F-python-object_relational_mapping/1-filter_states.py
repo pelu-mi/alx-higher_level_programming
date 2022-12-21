2d618f72162633abe64227f8df5ea96c0d7c5615
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2022-12-21 16:45:11 +0100

    Added file for task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Module for script that lists all states from the db hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    u_name = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    db = MySQLdb.connect(
+            user=u_name, passwd=pwd, database=db, host='localhost', port=3306)
+    c = db.cursor()
+
+    c.execute("""SELECT * FROM states
+              WHERE states.name LIKE 'N%'
+              ORDER BY states.id;""")
+
+    results = c.fetchall()
+    for i in results:
+        print(i)
