a5316841ebf87527ea5164546584f0907de77f9e
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-03 17:16:45 +0100

    Pycodestyle fix for task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,5 +14,6 @@ if __name__ == "__main__":
     # Create Session
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+    for instance in session.query(State).filter(State.name.like(
+                    '%a%')).order_by(State.id):
         print("{}: {}".format(instance.id, instance.name))
