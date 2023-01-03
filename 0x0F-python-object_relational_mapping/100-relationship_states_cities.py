f165e4e593c413c894ee40f1d2a26856c1b3b973
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-03 19:40:55 +0100

    Minor fix 2

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,6 +10,8 @@ from sqlalchemy import create_engine
 
 
 if __name__ == "__main__":
+    """ Code to run if file is not imported
+    """
     # Create Engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
              sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
