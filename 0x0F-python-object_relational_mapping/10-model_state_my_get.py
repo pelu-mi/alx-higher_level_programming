1c69768b99132a3135e9a43d1735ff33bfc3e2ba
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-03 17:22:57 +0100

    Added file for task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    # Create Engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create Session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State.id).filter(State.name == sys.argv[4])
+    if query.scalar():
+        print(query.scalar())
+    else:
+        print("Nothing")
