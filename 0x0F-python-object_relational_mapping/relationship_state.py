4022fc739ca23c2ba6adc5902d707d21e2f47b81
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-01-03 20:19:01 +0100

    testing 4

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
-"""
-This script defines a State class and
-a Base class to work with MySQLAlchemy ORM.
+""" Module documentation for State class definition
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State class
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The State id of the class
-        name (str): The State name of the class
-        cities (:obj:`City`): The Cities belongs to State
+    """ Declaration of the State class
+
+    Attr:
+        __tablename__ (str): Name of the table in the db
+        id (int): Id of each row
+        name (str): name of each row
     """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship(City, backref="state", cascade="all, delete")
