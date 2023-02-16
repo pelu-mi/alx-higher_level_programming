c08559ad99e7a5b1ede4bf48efa1d37eb181d968
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-16 17:30:22 +0100

    Added file for task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module containing script using urllib and sys
+"""
+
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    value = {'email': email}
+
+    data = parse.urlencode(value)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        res = response.read()
+        print(res.decode())
