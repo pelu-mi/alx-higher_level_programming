523e104c62a888207e13356c0567219d893e3241
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-16 16:58:14 +0100

    Created folder for Network 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Module containing script using urllib
+"""
+
+from urllib import request
+
+
+req = request.Request('https://alx-intranet.hbtn.io/status')
+with request.urlopen(req) as response:
+    res = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(res)))
+    print('\t- content:', res)
+    print('\t- utf8 content:', res.decode())
