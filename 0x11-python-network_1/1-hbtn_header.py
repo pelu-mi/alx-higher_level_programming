98116fc354120e87da09aaef26b7a96e667abf35
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-16 17:07:13 +0100

    Fix 2 for task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """ Module containing script using urllib and sys
+Python script that takes in a URL, sends a request to the URL
+and displays the value of the X-Request-Id variable found in
+the header of the response.
 """
 
 from urllib import request
