954b81c9775fec432715fd5fdbe3cff56dabe970
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-16 18:09:06 +0100

    Fix 8 for task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,18 +2,17 @@
 """Module containing script using urllib and sys
 """
 
-from urllib import request, parse
-from urllib.error import HTTPError
 import sys
+import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = request.Request(url)
+    request = urllib.request.Request(url)
     try:
-        with request.urlopen(req) as response:
-            res = response.read()
-            print(res.decode("utf-8"))
-    except HTTPError as e:
-        print("Error Code: {}".format(e.code))
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
