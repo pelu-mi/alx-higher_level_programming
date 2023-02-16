9b169bcb266ea3e95068cf31b88f73a270086ce8
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-16 17:16:12 +0100

    Fix 4 for task 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@
 from urllib import request
 
 
-req = request.Request('https://alx-intranet.hbtn.io/status')
-with request.urlopen(req) as response:
-    res = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(res)))
-    print('\t- content:', res)
-    print('\t- utf8 content:', res.decode())
+if __name__ == "__main__":
+    req = request.Request('https://alx-intranet.hbtn.io/status')
+    with request.urlopen(req) as response:
+        res = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(res)))
+        print('\t- content:', res)
+        print('\t- utf8 content:', res.decode())
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """Module containing script using urllib and sys
-Python script that takes in a URL, sends a request to the URL
-and displays the value of the X-Request-Id variable found in
-the header of the response."""
+"""
 
 from urllib import request
 import sys
 
 
-req = request.Request(sys.argv[1])
-with request.urlopen(req) as response:
-    res = response.info()
-    print('{}'.format(res['X-Request-Id']))
+if __name__ == "__main__":
+    req = request.Request(sys.argv[1])
+    with request.urlopen(req) as response:
+        res = response.info()
+        print('{}'.format(res.get('X-Request-Id')))
