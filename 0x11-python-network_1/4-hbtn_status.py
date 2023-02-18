4fdd52d194ac4d09f114ed920c7f84b84d206dfa
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-18 11:58:54 +0100

    Added file for task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module containing script using requests
+"""
+
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content:', r.text)
