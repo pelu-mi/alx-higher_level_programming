fbef3aa5efd46404d0bdfd1efa60e5503cb9c1d9
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-18 12:18:02 +0100

    Added file for task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,4 +9,4 @@ import sys
 if __name__ == "__main__":
     header_dict = {'email': sys.argv[2]}
     r = requests.post(sys.argv[1], data=header_dict)
-    print('Your email is: {}'.format(r.headers.get('email')))
+    print(r.text)
