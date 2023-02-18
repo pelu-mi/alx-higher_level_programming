ab2c8653af5e9f70ecd5d9911f0256ae1500a082
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-18 12:49:08 +0100

    Added file for task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Module containing script using requests and sys
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    try:
+        q = sys.argv[1]
+    except IndexError:
+        q = ""
+    data_sent = {'q': q}
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data_sent)
+    try:
+        info = r.json()
+    except requests.exceptions.JSONDecodeError:
+        print('Not a valid JSON')
+    else:
+        if info == {}:
+            print('No result')
+        else:
+            print('[{}] {}'.format(info.get('id'), info.get('name')))
