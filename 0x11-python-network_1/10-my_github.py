160c291882f5bac9ca06f5cabaf328eb92556aee
Oluwapelumi Fadolapo <pelumifadolapo7@gmail.com>
2023-02-18 16:36:32 +0100

    fix 1 for task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,21 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    pwd = sys.argv[2]
-    # Start from here
-
-    url = 'https://api.github.com/users/' + sys.argv[1]
-    auth = 'Bearer ' + sys.argv[2]
-    data_sent = {'Accept': 'application/vnd.github+json',
-                 'Authorization': auth,
-                 'X-GitHub-Api-Version': '2022-11-28'}
-    r = requests.get(url)
-    try:
-        info = r.json()
-        if info.get('message') != 'Bad credentials':
-            print('{}'.format(info.get('id')))
-        else:
-            print('None')
-    except ValueError:
-        print('None')
+    auth = requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=auth)
+    print('{}'.format(r.json().get('id')))
